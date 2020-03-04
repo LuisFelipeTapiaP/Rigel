364cfdc98496ae27a2d049a1b2e9ebaa31c4c23a
Luis Felipe Tapia <l.tapia.itms@gmail.com>
2020-03-04 15:17:27 -0300

    cambio de boostrap a semantic ui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import { Container, Header, List } from "semantic-ui-react";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import Example from "./example";
+
+const App = ({ children }) => (
+  <Container style={{ margin: 20 }}>
+    <Header as="h3">This example is powered by Semantic UI React 😊</Header>
+    <List bulleted>
+      <List.Item
+        as="a"
+        content="💌 Official documentation"
+        href="https://react.semantic-ui.com/"
+        target="_blank"
+      />
+      <List.Item
+        as="a"
+        content="💡 StackOverflow"
+        href="https://stackoverflow.com/questions/tagged/semantic-ui-react?sort=frequent"
+        target="_blank"
+      />
+    </List>
+
+    {children}
+  </Container>
+);
+
+// TODO: Switch to https://github.com/palmerhq/the-platform#stylesheet when it will be stable
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
+
+ReactDOM.render(
+  <App>
+    <Example />
+  </App>,
+  document.getElementById("root")
+);
