108c023222642be87afa9d41ac1dc512b86077ea
root <root@ip-172-31-47-28.us-east-2.compute.internal>
2020-03-04 03:41:08 +0000

    mod en server

---
+++ build/precache-manifest.e589ae524c036fee6bce6bcdf90acf14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1a2964c425b7fda5ae07456195d695c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55a2da29da0836690d0b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7057e61f35e92315b16f",
+    "url": "/static/js/2.0ef6727d.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.0ef6727d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a2da29da0836690d0b",
+    "url": "/static/js/main.f21b82cc.chunk.js"
+  },
+  {
+    "revision": "7000af948759a57844b4",
+    "url": "/static/js/runtime-main.f479105f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
